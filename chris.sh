e008da2ac2040d4ac721af982729d376eb928919
e <rekarger@gmail.com>
2017-02-07 19:19:58 -0500

    Update chris.sh

--- chris.sh
+++ chris.sh
@@ -3,7 +3,7 @@ echo "R3K7|-=-=-=_=l336=_=-=-=-|R3K7"
 name=$1
 while : 
     do
-    curl --url $name --connect-timeout 2 -s --no-buffer > /dev/null
-    N=$((N+1))
-    echo "Request #$N is it still up or is my 1337ness killing it?"
+        curl -A "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5" --url $name --connect-timeout 2 -s --no-buffer > /dev/null
+        N=$((N+1))
+        echo "Request #$N is it still up or is my 1337ness killing it?"
 done
